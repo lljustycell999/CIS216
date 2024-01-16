b697839db0a28d6fd1bafa74e11f3df91c2c1d29
lljustycell999 <123667513+lljustycell999@users.noreply.github.com>
2024-01-16 01:18:39 -0500

    Update and rename Parts Class 2.rtf to Parts.java

---
+++ CIS 216 Lab 08 - Car Parts Inventory (Updated with Modify & Delete Features)/Parts.java
@@ -0,0 +1,97 @@
+package lab.pkg08;
+
+public class Parts {
+    
+    private int partIDNumber;
+    private String partDescription;
+    private double partCost;
+    private double partPrice;
+    private double partWeight;
+    private String partLocation;
+    private int partQuantity;
+    
+    public void SetPartIDNumber(int somePartIDNumber) {
+        
+        partIDNumber = somePartIDNumber;
+    
+    }
+
+    public void SetPartDescription(String somePartDescription) {
+        
+       partDescription = somePartDescription;
+    
+    }
+
+    public void SetPartCost(double somePartCost) {
+        
+       partCost = somePartCost;
+    
+    }
+    
+    public void SetPartPrice(double somePartPrice) {
+        
+        partPrice = somePartPrice;
+    
+    }
+
+    public void SetPartWeight(double somePartWeight) {
+        
+        partWeight = somePartWeight;
+    
+    }
+
+    public void SetPartLocation(String somePartLocation) {
+        
+        partLocation = somePartLocation;
+    
+    }
+
+    public void SetPartQuantity(int somePartQuantity) {
+        
+        partQuantity = somePartQuantity;
+    
+    }
+    
+    public int GetPartIDNumber( ) {
+        
+        return partIDNumber;
+        
+    }
+    
+    public String GetPartDescription( ) {
+        
+        return partDescription;
+                
+    }
+    
+    public double GetPartCost( ) {
+        
+        return partCost;
+        
+    }
+    
+    public double GetPartPrice( ) {
+        
+        return partPrice;
+        
+    }
+    
+    public double GetPartWeight( ) {
+        
+        return partWeight;
+                
+    }
+    
+    public String GetPartLocation( ) {
+        
+        return partLocation;
+        
+    }
+    
+    public int GetPartQuantity( ) {
+        
+        return partQuantity;
+                
+    }
+    
+}
