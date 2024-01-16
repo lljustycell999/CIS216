98b204344b1a003a4796a2457047277278f5b854
lljustycell999 <123667513+lljustycell999@users.noreply.github.com>
2024-01-16 01:19:05 -0500

    Update and rename AppendParts Class 2.rtf to AppendParts.java

---
+++ CIS 216 Lab 08 - Car Parts Inventory (Updated with Modify & Delete Features)/AppendParts.java
@@ -0,0 +1,97 @@
+package lab.pkg08;
+
+public class AppendParts {
+    
+    private int newPartIDNumber;
+    private String newPartDescription;
+    private double newPartCost;
+    private double newPartPrice;
+    private double newPartWeight;
+    private String newPartLocation;
+    private int newPartQuantity;
+    
+    public void SetNewPartIDNumber(int anotherPartIDNumber) {
+        
+        newPartIDNumber = anotherPartIDNumber;
+    
+    }
+
+    public void SetNewPartDescription(String anotherPartDescription) {
+        
+       newPartDescription = anotherPartDescription;
+    
+    }
+
+    public void SetNewPartCost(double anotherPartCost) {
+        
+       newPartCost = anotherPartCost;
+    
+    }
+    
+    public void SetNewPartPrice(double anotherPartPrice) {
+        
+       newPartPrice = anotherPartPrice;
+    
+    }    
+
+    public void SetNewPartWeight(double anotherPartWeight) {
+        
+        newPartWeight = anotherPartWeight;
+    
+    }
+
+    public void SetNewPartLocation(String anotherPartLocation) {
+        
+        newPartLocation = anotherPartLocation;
+    
+    }
+
+    public void SetNewPartQuantity(int anotherPartQuantity) {
+        
+        newPartQuantity = anotherPartQuantity;
+    
+    }
+    
+    public int GetNewPartIDNumber( ) {
+        
+        return newPartIDNumber;
+        
+    }
+    
+    public String GetNewPartDescription( ) {
+        
+        return newPartDescription;
+                
+    }
+    
+    public double GetNewPartCost( ) {
+        
+        return newPartCost;
+        
+    }
+    
+    public double GetNewPartPrice( ) {
+        
+        return newPartPrice;
+        
+    }
+    
+    public double GetNewPartWeight( ) {
+        
+        return newPartWeight;
+                
+    }
+    
+    public String GetNewPartLocation( ) {
+        
+        return newPartLocation;
+        
+    }
+    
+    public int GetNewPartQuantity( ) {
+        
+        return newPartQuantity;
+                
+    }
+    
+}
